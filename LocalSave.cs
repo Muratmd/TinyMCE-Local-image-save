bedafa2ea1a0162dbba0b79a118a310458fd654b
Muratmd <46865642+Muratmd@users.noreply.github.com>
2022-05-15 09:59:16 +0300

    Create LocalSave.cs

---
+++ LocalSave.cs
@@ -0,0 +1,27 @@
+ public class LocalSave : Controller
+    {
+        [HttpGet("SaveTest")]
+        public ActionResult SaveTest()
+        {
+            return View();
+        }
+
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> FileUpload(IFormFile file)
+        {
+           
+            var random = "";
+            var name = file.FileName.ToLower();
+            if (file.Length > 0)
+            {
+                random = Guid.NewGuid() + name;
+                var filePath = Path.Combine("wwwroot/Images" , random);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+            return Ok(new {location = random});
+        }
+    }
